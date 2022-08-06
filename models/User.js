902a41e2606a0c4c087a99f37a68f0b3b9d9d8b0
Brandon Benson <brandonbenson09@gmail.com>
2022-08-05 21:28:23 -0400

    started models

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    // UNIQUE
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    // UNIQUE
+    // Must match a valid email address (look into Mongoose's matching validation)
+  },
+  //   thoughts: [_id values referencing the thought model],
+  //   friends: [_id values referencing the user model]
+});
