a4e51503d5374873c2f273f1f8d88a171c7617d2
Brandon Benson <brandonbenson09@gmail.com>
2022-08-06 19:28:57 -0400

    created thought schema in model

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,35 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const ThoughtSchema = new Schema({});
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
+// create THOUGHT model w thoughtSchema
+const Thought = model("Thought", thoughtSchema);
+
+// export model
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,21 +1,44 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    // UNIQUE
-    required: true,
-    trim: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [, "Please enter a valid email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    // UNIQUE
-    // Must match a valid email address (look into Mongoose's matching validation)
-  },
-  //   thoughts: [_id values referencing the thought model],
-  //   friends: [_id values referencing the user model]
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
+// create USER model w userSchema
+const User = model("User", userSchema);
+
+// export model
 model.exports = User;
