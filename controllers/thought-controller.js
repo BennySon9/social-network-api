9e5652ad4bd905ec318ce57e943593806df1ce80
Brandon Benson <brandonbenson09@gmail.com>
2022-08-06 21:19:17 -0400

    layed out controller methods

---
+++ controllers/thought-controller.js
@@ -0,0 +1,26 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+  // get ALL thoughts
+  getAllThoughts() {},
+
+  // get ONE thought by id
+  getThoughtById() {},
+
+  // CREATE a thought
+  createThought() {},
+
+  // UPDATE a thought by id
+  updateThought() {},
+
+  // DELETE a thought
+  deleteThought() {},
+
+  // ADD a reaction
+  addReaction() {},
+
+  // DELETE a reaction
+  deleteReaction() {},
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,26 @@
+const { User } = require("../models");
+const { populate } = require("../models/User");
+
+const userController = {
+  // get ALL users
+  getAllUsers() {},
+
+  // get ONE user by id
+  getUserById() {},
+
+  // CREATE user
+  createUser() {},
+
+  // UPDATE user by id
+  updateUser() {},
+
+  // DELETE a user by id
+  deleteUser() {},
+
+  // ADD a friend
+  addFriend() {},
+
+  // REMOVE a friend
+  removeFriend() {},
+};
+module.exports = userController;
